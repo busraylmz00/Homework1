f50df1c87a9e6b2c1ce29c4981bf0f02c6961715
Büşra Yılmaz <busra.yilmaz@trendyol.com>
2020-03-25 18:06:41 +0300

    ödev yapıldı

---
+++ Homework_1.py
@@ -0,0 +1,25 @@
+
+
+Toplama = 1
+Çıkarma = 2
+
+output = "İlk sayı: {}, ikinci sayı: {}"
+İşlem = int (input("Lütfen işlem seçiniz."))
+
+if İşlem == 1 :
+    birinci_input = int(input("Lütfen ilk sayıyı giriniz."))
+    ikinci_input = int(input("Lütfen ikinci sayıyı giriniz."))
+
+    print(output.format(birinci_input, ikinci_input), "için toplama işleminin sonucu", (birinci_input + ikinci_input))
+else:
+    print ("Yanlış seçim yaptınız.")
+
+if İşlem == 2 :
+    birinci_input = int(input("Lütfen ilk sayıyı giriniz."))
+    ikinci_input = int(input("Lütfen ikinci sayıyı giriniz."))
+
+    print(output.format(birinci_input, ikinci_input), "için çıkarma işleminin sonucu", (birinci_input - ikinci_input))
+else:
+    print ("Yanlış seçim yaptınız.")
+
+
---
+++ if_else.py
@@ -0,0 +1,18 @@
+
+plaka_kod = int (input ("Lütfen plaka kodu giriniz."))
+
+if plaka_kod == 34:
+    print ("Girmiş olduğunuz 34 plakası İstanbul iline aittir")
+
+elif plaka_kod == 28:
+    print("Girmiş olduğunuz 28 plakası Giresun iline aittir")
+
+elif plaka_kod == 54:
+    print("Girmiş olduğunuz 54 plakası Sakarya iline aittir")
+
+else:
+    print ("Girmiş olduğunuz plaka kodu sistemde bulunmamaktadır.")
+
+
+
+

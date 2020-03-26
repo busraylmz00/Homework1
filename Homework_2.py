68683b46fcfc5756ee7db0829fa8000c2ca405f1
Büşra Yılmaz <busra.yilmaz@trendyol.com>
2020-03-26 19:12:06 +0300

    homework fixed.

--- Homework_2.py
+++ Homework_2.py
@@ -4,17 +4,17 @@ max_number = 100
 
 user_forecast = int (input ("1-100 arasında bir sayı tuttum. Bu sayıyı bilebilir misin?"))
 while user_forecast > max_number :
-    print ("Lütfen 1-100 arasında sayı giriniz.\nTekrar deneyiniz...")
+    print ("Lütfen 1-100 arasında sayı giriniz.")
     break
-
-if user_forecast > my_number and user_forecast < max_number :
-    print("Lütfen daha küçük sayı tahmin ediniz.")
-
-elif user_forecast < my_number :
-    print ("Lütfen daha büyük sayı tahmin ediniz.")
-
-elif user_forecast == my_number :
+while user_forecast > my_number and user_forecast < max_number :
+    print("Lütfen daha küçük sayı tahmin ediniz.\nTekrar deneyiniz...")
+    break
+while  user_forecast < my_number :
+    print ("Lütfen daha büyük sayı tahmin ediniz.\nTekrar deneyiniz...")
+    break
+while user_forecast == my_number :
     print("Tebrikler! Doğru tahmin ettiniz.\nProgram kapatılıyor...")
+    break
 
 
 

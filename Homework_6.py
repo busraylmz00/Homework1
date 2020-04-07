0ca0912cb35d4483b2f1b73e2e0e39246cf28ea8
Büşra Yılmaz <busra.yilmaz@trendyol.com>
2020-04-07 14:07:05 +0300

    homework fixed

--- Homework_6.py
+++ Homework_6.py
@@ -11,18 +11,21 @@ while user_name :
         if forbidden_name == user_input.replace("Admin","admin"):
             raise ValueError
 
-        for number in user_input:
-            if (number >='0' and number<='9'):
+        for number in range(0, 10):
+            if str(number) in user_input:
                 raise TypeError
 
     except ValueError :
         print("Kullanıcı adı \"admin\" olamaz.")
 
     except TypeError :
-        print("Kullanıcı adı rakam içeremez.")
+        print("Kullanıcı adı rakam içeremez")
 
     else :
         print("Tebrikler! Kullanıcı adınız kaydedildi.")
         user_name = False
+
+
+
 
 
---
+++ Homework_7.py
@@ -0,0 +1,10 @@
+
+user_name ="trendyol"
+
+for number in range (0,10) :
+    if str(number) in user_name :
+        print("rakam içeremez.")
+
+
+for number in "büşra" :
+        print(number)
